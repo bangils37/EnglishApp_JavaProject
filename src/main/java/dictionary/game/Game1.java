5af19cf27e27ece83ec317af4b529aac9a8944bf
luuhuythanh0111 <124591525+luuhuythanh0111@users.noreply.github.com>
2023-12-13 13:35:00 +0700

    + fixed close button

--- src/main/java/dictionary/game/Game1.java
+++ src/main/java/dictionary/game/Game1.java
@@ -1,13 +1,19 @@
 package dictionary.game;
 
+import dictionary.ui.controller.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Random;
 
@@ -171,6 +177,37 @@ public class Game1 {
                 DtextField.setText(target);
                 correctButton = DButton;
                 break;
+        }
+    }
+
+    @FXML
+    public void changeToApplication(ActionEvent event) {
+        loadMainApplicationScene(event);
+    }
+
+    private void loadMainApplicationScene(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(
+                    Objects.requireNonNull(
+                            getClass()
+                                    .getClassLoader()
+                                    .getResource("fxml/Application.fxml")));
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            scene.getStylesheets()
+                    .add(
+                            Objects.requireNonNull(
+                                            getClass()
+                                                    .getResource(
+                                                            (Application.isLightMode()
+                                                                    ? "/css/Application-light.css"
+                                                                    : "/css/Application-dark.css")))
+                                    .toExternalForm());
+            appStage.setTitle("Dictionary");
+            appStage.setScene(scene);
+            appStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
